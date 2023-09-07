8ab2f0d70b4db7132d38788954d6bc589b4acaaf
Tetroner9 <omkadam2125@gmail.com>
2023-09-07 20:08:22 +0530

    IDE

--- morse/new.py
+++ morse/new.py
@@ -4,44 +4,45 @@ from playsound import playsound
 
 # Define Morse code dictionaries
 morse_code_dict_1 = {
-    '.-': 'A',   '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
     '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
-    '-.-': 'K',  '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
     '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
-    '..-': 'U',  '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
     '--..': 'Z',
-    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', 
-    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', 
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
     ' ': ' '
 }
 morse_code_dict_2 = {
-    '.-': 'Z',   '-...': 'Y', '-.-.': 'X', '-..': 'W', '.': 'V',
+    '.-': 'Z', '-...': 'Y', '-.-.': 'X', '-..': 'W', '.': 'V',
     '..-.': 'U', '--.': 'T', '....': 'S', '..': 'R', '.---': 'Q',
-    '-.-': 'P',  '.-..': 'O', '--': 'N', '-.': 'M', '---': 'L',
+    '-.-': 'P', '.-..': 'O', '--': 'N', '-.': 'M', '---': 'L',
     '.--.': 'K', '--.-': 'J', '.-.': 'I', '...': 'H', '-': 'G',
-    '..-': 'F',  '...-': 'E', '.--': 'D', '-..-': 'C', '-.--': 'B',
+    '..-': 'F', '...-': 'E', '.--': 'D', '-..-': 'C', '-.--': 'B',
     '--..': 'A',
-    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', 
-    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', 
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
     ' ': ' '
 }
 
 morse_code_dict_3 = {
-    '.-': 'M',   '-...': 'N', '-.-.': 'V', '-..': 'C', '.': 'Z',
+    '.-': 'M', '-...': 'N', '-.-.': 'V', '-..': 'C', '.': 'Z',
     '..-.': 'L', '--.': 'O', '....': 'P', '..': 'Y', '.---': 'E',
-    '-.-': 'J',  '.-..': 'U', '--': 'B', '-.': 'Q', '---': 'D',
+    '-.-': 'J', '.-..': 'U', '--': 'B', '-.': 'Q', '---': 'D',
     '.--.': 'T', '--.-': 'A', '.-.': 'W', '...': 'R', '-': 'S',
-    '..-': 'I',  '...-': 'F', '.--': 'G', '-..-': 'X', '-.--': 'H',
+    '..-': 'I', '...-': 'F', '.--': 'G', '-..-': 'X', '-.--': 'H',
     '--..': 'K',
-    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', 
-    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', 
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
     ' ': ' '
 }
+
 
 # Function to select a random dictionary
 def select_random_dictionary():
     random_n = random.randint(1, 100)
-    random_num = (random_n%3)+1
+    random_num = (random_n % 3) + 1
     print(f"Dictionary:{random_num}")
     # Generate a random number between 1 and 3
     if random_num == 1:
@@ -52,12 +53,11 @@ def select_random_dictionary():
         return morse_code_dict_3
 
 
-
 # Function to generate a random alphabetical password
-#def generate_random_password(length):
-    #alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    #return ''.join(random.choice(alphabet) for _ in range(length))
-   
+# def generate_random_password(length):
+# alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+# return ''.join(random.choice(alphabet) for _ in range(length))
+
 
 # Function to encode text in Morse code
 def text_to_morse(text, selected_dict):
@@ -73,6 +73,7 @@ def text_to_morse(text, selected_dict):
                     break
     return ' '.join(morse_code)
 
+
 # Function to decode Morse code to text
 def morse_to_text(morse_code, selected_dict):
     morse_code = morse_code.split(' ')
@@ -84,6 +85,7 @@ def morse_to_text(morse_code, selected_dict):
             text += ' '
     return text
 
+
 # Function to play Morse code as audio
 def play_morse(message):
     for char in message:
@@ -94,12 +96,13 @@ def play_morse(message):
             playsound("long.mp3")
             time.sleep(0.3)  # Sleep for 0.3 seconds (adjust as needed)
         elif char == " ":
-            time.sleep(1.5) 
+            time.sleep(1.5)
+
 
 while True:
     select_dict = select_random_dictionary()
     password = input("CODE: ")
-    morse_code = text_to_morse(password,select_dict)
+    morse_code = text_to_morse(password, select_dict)
     print(f"MORSE CODE:{morse_code}")
     i = True
     try:
@@ -108,6 +111,6 @@ while True:
             playsound("stathicc.wav")
             time.sleep(0.5)
     except KeyboardInterrupt:
-        i =False
-        
+        i = False
+
     print("CYCLE COMPLETED.")
